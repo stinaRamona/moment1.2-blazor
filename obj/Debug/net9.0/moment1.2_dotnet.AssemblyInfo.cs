a017e3569fd0be2efc1d7943d346af0020e35fef
Stina Persson <stpe1901@student.miun.se>
2025-01-29 09:53:30 +0100

    mindre änringar

--- obj/Debug/net9.0/moment1.2_dotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/moment1.2_dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moment1.2_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a1e3dbcf4ec1b2633e70f9b015a3e048ffcfdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a28928e7f0008a6ca46bf823120f8e39387b62")]
 [assembly: System.Reflection.AssemblyProductAttribute("moment1.2_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moment1.2_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
